b85aafe1e0c940d5f742583fbbc9e55b02aa67fa
Artur Pabjańczyk <artur@pabjanczyk.pl>
2020-10-05 21:34:32 +0200

    Extract App component from index js

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import {EditableTimeBox, TimeBoxList} from "./remaining-components";
+
+function App() {
+    return (
+        <div className="App">
+            <EditableTimeBox/>
+            <TimeBoxList/>
+        </div>
+    )
+}
+
+export default App;
