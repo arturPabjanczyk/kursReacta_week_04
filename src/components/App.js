8ddbf58500d7c5de7d3688d9ead7d557701decca
Artur Pabjańczyk <artur@pabjanczyk.pl>
2020-10-10 20:44:50 +0200

    Use error handling component to catch errors in whole app

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,16 @@ import React from "react";
 
 import EditableTimeBox from "./EditableTimeBox";
 import TimeBoxList from "./TimeBoxList";
+import Error from "./Error";
 
 function App() {
     return (
         <React.StrictMode>
             <div className="App">
-                <EditableTimeBox/>
-                <TimeBoxList/>
+                <Error message="Coś nie działa w całej aplikacji - wyślij email do support@artapps.pl">
+                    <EditableTimeBox/>
+                    <TimeBoxList/>
+                </Error>
             </div>
         </React.StrictMode>
     )
