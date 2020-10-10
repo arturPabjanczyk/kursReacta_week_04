a0b098a6c0d7e9642f253a16f4a3381cbecd3389
Artur Pabjańczyk <artur@pabjanczyk.pl>
2020-10-10 19:26:25 +0200

    Handle error in dedicated method

--- src/components/TimeBox.js
+++ src/components/TimeBox.js
@@ -3,7 +3,7 @@ import React from "react";
 function TimeBox({onEdit, onDelete, timeBox}) {
 
         if (timeBox.totalTimeInMinutes <=0) {
-                throw new Error("Całkowity czad musi być większy od 0")
+                throw new Error("Całkowity czas musi być większy od 0")
         }
 
         return (
