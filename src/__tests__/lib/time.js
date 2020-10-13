59f232dbc2256d062577e82d9a14f866d2cf8e0c
Artur Pabjańczyk <artur@pabjanczyk.pl>
2020-10-13 12:53:40 +0200

    Refactor tests

--- src/__tests__/lib/time.js
+++ src/__tests__/lib/time.js
@@ -1,25 +1,25 @@
 import {getMinutesAndSecondsFromDurationInSeconds} from "../../lib/time"
 
 describe("getMinutesAndSecondsFromDurationInSeconds", () => {
-    test("works for 30 seconds", () => {
+    it("returns 0 minutes and 30 seconds for 30 second duration", () => {
         expect(
             getMinutesAndSecondsFromDurationInSeconds(30)
         ).toEqual([0,30])
     })
 
-    test("returns 30 seconds for 30 second duration", () => {
+    it("returns 30 seconds for 30 second duration", () => {
         expect(
             getMinutesAndSecondsFromDurationInSeconds(30)[1]
         ).toBe(30)
     })
 
-    test("returns 0 minutes for 30 second duration", () => {
+    it("returns 0 minutes for 30 second duration", () => {
         expect(
             getMinutesAndSecondsFromDurationInSeconds(30)[0]
         ).toBe(0)
     })
 
-    test("works for 140 seconds", () => {
+    it("returns 2 minutes and 20 seconds for 140 second duration", () => {
         expect(
             getMinutesAndSecondsFromDurationInSeconds(140)
         ).toEqual([2,20])
