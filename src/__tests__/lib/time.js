719d8b2bfa2afb26ed2d82bfb179ec281175ffc3
Artur Pabjańczyk <artur@pabjanczyk.pl>
2020-10-13 12:45:30 +0200

    Group tests

--- src/__tests__/lib/time.js
+++ src/__tests__/lib/time.js
@@ -1,25 +1,27 @@
 import {getMinutesAndSecondsFromDurationInSeconds} from "../../lib/time"
 
-test("getMinutesAndSecondsFromDurationInSeconds works for 30 seconds", () => {
-    expect(
-        getMinutesAndSecondsFromDurationInSeconds(30)
-    ).toEqual([0,30])
-})
+describe("getMinutesAndSecondsFromDurationInSeconds", () => {
+    test("works for 30 seconds", () => {
+        expect(
+            getMinutesAndSecondsFromDurationInSeconds(30)
+        ).toEqual([0,30])
+    })
 
-test("getMinutesAndSecondsFromDurationInSeconds returns 30 seconds for 30 second duration", () => {
-    expect(
-        getMinutesAndSecondsFromDurationInSeconds(30)[1]
-    ).toBe(30)
-})
+    test("returns 30 seconds for 30 second duration", () => {
+        expect(
+            getMinutesAndSecondsFromDurationInSeconds(30)[1]
+        ).toBe(30)
+    })
 
-test("getMinutesAndSecondsFromDurationInSeconds returns 0 minutes for 30 second duration", () => {
-    expect(
-        getMinutesAndSecondsFromDurationInSeconds(30)[0]
-    ).toBe(0)
-})
+    test("returns 0 minutes for 30 second duration", () => {
+        expect(
+            getMinutesAndSecondsFromDurationInSeconds(30)[0]
+        ).toBe(0)
+    })
 
-test("getMinutesAndSecondsFromDurationInSeconds works for 140 seconds", () => {
-    expect(
-        getMinutesAndSecondsFromDurationInSeconds(140)
-    ).toEqual([2,20])
+    test("works for 140 seconds", () => {
+        expect(
+            getMinutesAndSecondsFromDurationInSeconds(140)
+        ).toEqual([2,20])
+    })
 })
