91ee9769f02ed09c26dca4991f307a5035320cd7
Artur Pabjańczyk <artur@pabjanczyk.pl>
2020-10-14 13:12:01 +0200

    Test component rendering in one test

--- src/__tests__/components/clock.js
+++ src/__tests__/components/clock.js
@@ -11,14 +11,9 @@ describe('<Clock />', () => {
                 <Clock minutes={10} seconds={20}/>
             )
         })
-        it('should render a h2 element', () => {
-            console.log(clockRenderer.toJSON());
+        it('shouls render properly', () => {
             expect(clockRenderer.toJSON().type).toEqual("h2");
-        });
-        it('should set a Clock className', () => {
             expect(clockRenderer.toJSON().props).toMatchObject({"className": expect.stringMatching(/Clock/)});
-        });
-        it('should render time properly', () => {
             expect(clockRenderer.toJSON().children[1].children).toEqual(expect.arrayContaining(["10"]));
         });
     });
