51b81f4d0d9d320160be39de6a325b597ac4f906
Artur Pabjańczyk <artur@pabjanczyk.pl>
2020-10-25 17:45:04 +0100

    Add ProgressArch animated

--- src/components/ProgressArch.js
+++ src/components/ProgressArch.js
@@ -4,14 +4,21 @@ class ProgressArch extends React.Component {
   constructor(props) {
     super(props);
     this.canvas = React.createRef();
+    this.opacityUp = true;
+    this.opacity = 0;
   }
 
   componentDidMount() {
     this.draw();
+    this.animationRequestId = window.requestAnimationFrame(this.update)
   }
 
   componentDidUpdate() {
     this.draw();
+  }
+
+  componentWillUnmount() {
+    window.cancelAnimationFrame(this.animationRequestId);
   }
 
   draw() {
@@ -27,11 +34,23 @@ class ProgressArch extends React.Component {
     ctx.stroke(shape);
 
     const segment = new Path2D();
-    ctx.fillStyle = "orangered";
+    ctx.fillStyle = `rgba(255, 69, 0, ${this.opacity})`;
     segment.arc(150, 150, 150, Math.PI, Math.PI * (1 + percent / 100), false);
     segment.lineTo(150, 150);
     segment.lineTo(0, 150);
     ctx.fill(segment);
+  }
+
+  update = () => {
+    this.opacity = this.opacityUp ? this.opacity + 0.01 : this.opacity - 0.01;
+    if (this.opacity > 1) {
+      this.opacityUp = false;
+    }
+    if (this.opacity < 0.2) {
+      this.opacityUp = true;
+    }
+    this.draw();
+    this.animationRequestId = window.requestAnimationFrame(this.update);
   }
 
   render() {
